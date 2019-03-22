59c7e91f291e24c932b913f16257d6a8b6d66034
Snegil <34574057+Snegil@users.noreply.github.com>
2019-03-22 09:53:53 +0100

    Fixade

--- index.php
+++ index.php
@@ -13,9 +13,16 @@
 	<body>
 		<div id="topbox">
 			<a href="index.php">
-				<img id="logo" src="images/Loggan.png">
+				<img id="logo" src="images/Loggan.png">	
 			</a>
+			<span id="autismContainer">
+				<a target="_blank" href="https://www.autism.se/">
+					<img id="autism" src="images/Autism.png">
+				</a>
+			</span>
 		</div>
+		Vector Art by <a target="_blank" href="https://vecteezy.com">Vecteezy.com</a>
+		Background by <a target="_blank" href="https://www.svgbackgrounds.com/">SVGBackgrounds</a>
 	</body>
 </html>
 		
