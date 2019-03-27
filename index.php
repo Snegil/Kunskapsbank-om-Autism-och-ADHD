dac3ce893358dde02b189862c17ad5afd029298b
Snegil <144wallfin@gmail.com>
2019-03-27 20:20:12 +0100

    Organiserade filer

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta charset="utf-8" lang="sv">
 		<title>
-		Kunskapsbank - Hem	
+		Kunskapsbank - Hem
 		</title>
 		<link rel='shortcut icon' type='image/x-icon' href='favicon.ico' />
-		<link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 		<link rel="stylesheet" href="stylesheet.css">
 		<link rel="stylesheet" href="adaptation.css">
 		<link rel="stylesheet" type="text/css" href="normalize.css">
@@ -16,14 +17,14 @@
 	<body>
 		<div id="topbox">
 			<a href="index.php">
-				<img id="logo" src="images/Loggan.png">	
+				<img id="logo" src="images/Loggan.png">
 			</a>
 			<span id="autismContainer">
 				<a target="_blank" href="https://www.autism.se/">
-					<img id="autism" src="images/Autismband.png">
+					<img id="autism" src="images/AutismBild/Autismband.png">
 				</a>
 			</span>
-			
+
 		</div>
 		<span id="navigation">
 			<a class="button active" href="index.php">Hem</a>
@@ -38,23 +39,15 @@
 				</div>
 				<div id="introduction-text">
 					
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id venenatis neque, id ornare odio. In et erat urna. Donec facilisis, velit ut gravida vulputate, velit arcu convallis justo, at consectetur quam augue vitae lacus. In dapibus et nisi tempor ultrices. Donec sed convallis sem. Quisque quis lorem diam. Nulla vehicula mauris in ante condimentum molestie.
-
-Praesent blandit, lectus at consequat pellentesque, ligula urna condimentum ex, et hendrerit felis purus eget elit. Nulla at eros laoreet, sagittis tortor id, finibus nisl. Cras orci nibh, porta quis feugiat vel, aliquam vel mauris. Nulla vel ex et ex suscipit sodales ac sed nulla. Nunc bibendum nisi eu scelerisque ultrices. Cras vel euismod urna, sit amet sagittis mi. Praesent ultricies ultricies viverra. Morbi sollicitudin, lacus et vehicula hendrerit, magna libero vehicula velit, eget faucibus lacus metus non felis.
-
-Nulla et aliquam turpis, a dapibus diam. Donec tristique efficitur urna a efficitur. Sed egestas sapien pellentesque tellus lobortis scelerisque. Aliquam accumsan pellentesque nulla, non pretium nibh facilisis in. Duis vestibulum nec urna sit amet elementum. Donec eget rutrum ante, in malesuada felis. Praesent euismod neque a odio fringilla rhoncus. Nam mattis neque sit amet orci dictum, et congue mi tristique. Sed at aliquet odio, ut sollicitudin felis. Nunc aliquet blandit neque. Proin arcu massa, elementum eu tellus non, aliquam tristique nulla.
-
-Vestibulum non tincidunt neque. Suspendisse suscipit auctor mi, quis tincidunt sapien pulvinar egestas. Vestibulum maximus fermentum interdum. Nam nec dolor ac ante semper accumsan at a ante. Nullam enim magna, volutpat nec eros at, mollis consequat velit. Etiam libero tortor, pharetra at egestas at, congue sed ligula. Suspendisse potenti. Nullam vel dui massa. Morbi convallis massa sem, malesuada tincidunt orci ultricies et. Donec sollicitudin venenatis finibus. Donec vulputate lacus ac lobortis dictum. Sed sit amet tellus arcu. Donec viverra sed quam in volutpat. Etiam porttitor ipsum ac malesuada mollis. Pellentesque dui erat, laoreet vel ante id, ultrices accumsan neque.
-
-Suspendisse viverra malesuada porta. Fusce mollis, tortor vitae eleifend consectetur, augue metus pretium sem, vel ultrices leo orci ut enim. Nam dolor ante, sodales quis eros pharetra, pharetra facilisis enim. Curabitur turpis leo, egestas ac tortor ut, tincidunt dapibus neque. Quisque placerat, mi in hendrerit condimentum, lorem arcu varius justo, at molestie est dolor at nunc. Curabitur justo tortor, molestie at turpis et, vulputate porttitor quam. Proin nec tempus nunc. Praesent mollis sollicitudin congue. Donec vitae tellus nisl. 
 				</div>
 			</div>
 		</div>
 		<div id="footer">
+			<img id="ArbeteSkylt" src="images/ArbeteSkylt.png">
+			Pågående Projekt |
+
 			Vector Art från <a target="_blank" href="https://vecteezy.com">Vecteezy.com</a> |
 			Bakgrund från <a target="_blank" href="https://www.svgbackgrounds.com/">SVGBackgrounds</a>
 		</div>
 	</body>
 </html>
-		
